d7962343111b973ecd2a66d44533e8dcb4d60699
Yifei Kong <kong@yifei.me>
2022-03-02 00:18:11 +0800

    WIP: save at 2022-03-02 00:18:11

--- src/image_backup.ts
+++ src/image_backup.ts
@@ -1,28 +1,64 @@
 import axios from 'axios'
 import {promises as fs} from 'fs'
+import path from 'path'
+import mkdirp from 'mkdirp'
+import glob from 'fast-glob'
 
 
 const BASE_URL = 'https://sg.storage.bunnycdn.com'
+const DOWNLOAD_DIR = './_data'
 
-const visit = async (path: string) => {
-  const res = await axios.get(`${BASE_URL}/${path}`, {
-      headers: {
-        Accept: '*/*',
-        AccessKey: '0b17b3e3-6da6-40e3-8a574487998c-287f-443b'
-      }
-    })
+
+const visit = async (filepath: string) => {
+  const res = await axios.get(`${BASE_URL}${filepath}`, {
+    headers: {
+      Accept: '*/*',
+      AccessKey: '0b17b3e3-6da6-40e3-8a574487998c-287f-443b'
+    }
+  })
 
   for (const item of res.data) {
-    const {Path: path, IsDirectory: isDirecotry} = item
+    const {ObjectName: objectName, IsDirectory: isDirecotry} = item
+      const newPath = `${filepath}${objectName}`
     if (isDirecotry) {
-      await visit(path)
+      console.log(`visiting new directory: ${newPath}`)
+      await visit(newPath + "/")
     } else {
-      await fs.writeFile(path, res.data)
+      console.log(`downloading file: ${newPath}`)
+      const shortPath = newPath.replace("/onefly/", "")
+      const url = `https://onefly.b-cdn.net/${shortPath}`
+      const imageRes = await axios.get(url, {
+        responseType: 'arraybuffer'
+      })
+      const fullpath = `${DOWNLOAD_DIR}/${shortPath}`
+      const dirname = path.dirname(fullpath)
+      console.log(`making dir ${dirname}`)
+      await mkdirp(dirname)
+      await fs.writeFile(fullpath, imageRes.data)
+    }
+  }
+}
+
+
+const downloadSina = async () => {
+  const files = await glob("../notes/**/*.md")
+  let posts = []
+  for (const file of files) {
+    const fileContent = await fs.readFile(file, "utf-8")
+    const lines = fileContent.split("\n")
+    for (const [idx, line] of lines.entries()) {
+      // https://ws1.sinaimg.cn/large/006tNc79gy1fmjv6ugbr4j31jw0mo4qp.jpg
+      const m = line.match(/https:\/\/\w+\.sinaimg\.cn\/large\/[\w\.]+/)
+      if (m) {
+        const url = m[0]
+        const imageRes = await axios.get(url, {responseType: "arraybuffer"})
+        
+
+      }
     }
   }
 
-  console.log(res.data)
 }
 
-visit("/onefly/notes/")
-
+// visit("/onefly/notes/")
+downloadSina()
